a2a0e8244f05e3328c0196cc466b2879ae418ee6
Kolayagnesh <153358154+Kolayagnesh@users.noreply.github.com>
2025-05-27 23:24:54 +0530

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 const path = require('path');
 app.use(express.static(path.join(__dirname)));
 // Use a simple in-memory database for demo (replace with MongoDB/Postgres for production)
